44405dbedf2cb2e4b6f0977ae6bece5ea160391a
Pawan Bondre <119318111+pawanbondre67@users.noreply.github.com>
2024-01-10 15:43:47 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,3 +75,28 @@ window.addEventListener('scroll' ,scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    opacity: 1,
+    scale: 1.1 ,
+    duration: 2500 ,
+    delay: 300 ,
+   reset: true, //animation repeat
+ })
+
+ sr.reveal(`.home__data , .about__img , .about__data , .visit__data`) 
+
+ sr.reveal(`.home__image , .footer__img-1 , .footer__img-2`, {rotate: {z: -15} })
+ sr.reveal(`.home__bread , .about__bread`, {rotate: {z: 15} })
+ sr.reveal(`.home__footer`, { scale: 1 ,origin: 'bottom'})
+
+ sr.reveal(`.new__card:nth-child(1) img` , { rotate: {z: -30}, distance : 0})
+ sr.reveal(`.new__card:nth-child(2) img`, { rotate: {z: 15}, distance : 0 , delay: 600})
+ sr.reveal(`.new__card:nth-child(3) img` , { rotate: {z: -30}, distance : 0 ,delay: 900})
+
+  sr.reveal(`.favorite__card img`, { interval : 100 , rotate: {z: 15}, distance : 0})
+
+  sr.reveal(`.footer__container`, { scale:1})
+
+
